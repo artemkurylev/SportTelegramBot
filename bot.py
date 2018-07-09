60ecb651db3d42b8b7f98aca36c7665dc2414be6
artemkurylev <artembonapart@gmail.com>
2018-07-09 07:38:30 +0300

    Исправлено подключение библиотеки psycong2

--- bot.py
+++ bot.py
@@ -35,14 +35,14 @@ class BotHandler:
 myBot = BotHandler('554026510:AAHQahqhMAwffu4qWQSa0JZPJjDLamDtXO4')
 greetings = ('здравствуй', 'привет', 'ку', 'здорово')
 now = datetime.datetime.now()
-params = {
-  'database': 'dftilqnoe4t5kg',
-  'user': 'jkcvzflxvnhqcq',
-  'password': '80af35cf40ad6392d412671a379797fe9891cce47e525b87863aaa9728f0f784',
-  'host': 'ec2-54-227-243-210.compute-1.amazonaws.com',
-  'port': 5324
+dbparams = {
+    'host': 'ec2-54-227-243-210.compute-1.amazonaws.com',
+    'port': 5432,
+    'database': 'dftilqnoe4t5kg',
+    'user': 'jkcvzflxvnhqcq',
+    'password': '80af35cf40ad6392d412671a379797fe9891cce47e525b87863aaa9728f0f784',
 }
-db = psycopg2.connect(**params)
+db = psycopg2.connect(**dbparams)
 
 
 def main():
