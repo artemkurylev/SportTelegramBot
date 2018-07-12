2259bd3396409d014fb61c2250c6aae2dd6289ff
artemkurylev <artembonapart@gmail.com>
2018-07-12 18:13:35 +0300

    Добавлена Бд

--- bot.py
+++ bot.py
@@ -2,8 +2,16 @@ import requests
 import datetime
 import telebot
 import config
-
+import psycopg2
 bot = telebot.TeleBot(config.token)
+params = {
+  'database': 'dftilqnoe4t5kg',
+  'user': 'jkcvzflxvnhqcq',
+  'password': '80af35cf40ad6392d412671a379797fe9891cce47e525b87863aaa9728f0f784',
+  'host': 'ec2-54-227-243-210.compute-1.amazonaws.com',
+  'port': 5432
+}
+db = psycopg2.connect(**params)
 
 
 @bot.message_handler(content_types=["text"])
