4c751053860f504ee7209a81927fc0cb1fb77cfe
artemkurylev <artembonapart@gmail.com>
2018-07-12 18:43:30 +0300

    Соединение с Дб

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import datetime
 import telebot
 import config
 import psycopg2
+
 bot = telebot.TeleBot(config.token)
 params = {
   'database': 'dftilqnoe4t5kg',
@@ -12,11 +13,19 @@ params = {
   'port': 5432
 }
 db = psycopg2.connect(**params)
+cur = db.cursor()
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(commands=["start"])
+def remember_user(message):
+    record = cur.execute("Select * from users WHERE id=(%s)", message.chat.id)
+    if record is None:
+        cur.execute("Insert into users values (%s,%s)", [message.chat.id, message.chat.first_name])
 
 
 if __name__ == '__main__':
